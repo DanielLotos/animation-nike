349c880e5f58455c1d43135034e52da8edebaa2e
DanielLotos <daniellika@yandex.ru>
2018-05-26 02:03:25 +0300

    Сделать иконочный bg

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,17 @@
-console.log('Test JS import');
+const btnInvertLogin = document.querySelector('.form__forgot--login');
+const btnInvertReset = document.querySelector('.form__forgot--reset');
+const formLogin = document.querySelector('.login-form--login');
+const formReset = document.querySelector('.login-form--reset');
+const formFlipper = document.querySelector('.login-form__flipper');
+
+btnInvertLogin.addEventListener('click', function (event) {
+  event.preventDefault ? event.preventDefault() : (event.returnValue = false);
+  formFlipper.classList.remove('login-form__flipper--unflip');
+  formFlipper.classList.add('login-form__flipper--flip');
+});
+
+btnInvertReset.addEventListener('click', function (event) {
+  event.preventDefault ? event.preventDefault() : (event.returnValue = false);
+  formFlipper.classList.remove('login-form__flipper--flip');
+  formFlipper.classList.add('login-form__flipper--unflip');
+});

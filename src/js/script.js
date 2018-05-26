e1c68d4b38c8415d6fec519f9c3bef20a0fa92b7
DanielLotos <daniellika@yandex.ru>
2018-05-26 04:16:51 +0300

    Сделать мобильную версию

--- src/js/script.js
+++ src/js/script.js
@@ -3,15 +3,24 @@ const btnInvertReset = document.querySelector('.form__forgot--reset');
 const formLogin = document.querySelector('.login-form--login');
 const formReset = document.querySelector('.login-form--reset');
 const formFlipper = document.querySelector('.login-form__flipper');
+const iconsWrapper = document.querySelector('.icons');
+
+function removeClass () {
+  iconsWrapper.classList.remove('icons--scale');
+}
 
 btnInvertLogin.addEventListener('click', function (event) {
   event.preventDefault ? event.preventDefault() : (event.returnValue = false);
   formFlipper.classList.remove('login-form__flipper--unflip');
   formFlipper.classList.add('login-form__flipper--flip');
+  iconsWrapper.classList.add('icons--scale');
+  setTimeout(removeClass, 2000);
 });
 
 btnInvertReset.addEventListener('click', function (event) {
   event.preventDefault ? event.preventDefault() : (event.returnValue = false);
   formFlipper.classList.remove('login-form__flipper--flip');
   formFlipper.classList.add('login-form__flipper--unflip');
+  iconsWrapper.classList.add('icons--scale');
+  setTimeout(removeClass, 2000);
 });
